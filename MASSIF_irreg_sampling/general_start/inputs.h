1b95a1b69316cb04d629cd125a010ea05894e40a
Anuva Kulkarni <anuva.kulkarni@gmail.com>
2020-05-30 10:13:06 -0400

    matlab code to compare cufft and fftw visually

--- MASSIF_irreg_sampling/general_start/inputs.h
+++ MASSIF_irreg_sampling/general_start/inputs.h
@@ -1,15 +1,15 @@
-#define NX 64
-#define NY 64
-#define NZ 64
+#define NX 256
+#define NY 256
+#define NZ 256
 #define K 16 //dimension of small cube
-#define Kprime 56//should be NZ - K, for current example
+#define Kprime 240//should be NZ - K, for current example
 #define startX 4 //start points of domain
 #define startY 4
-#define startZ 4
-#define B 1024 // Number of pencils in one batch
+#define startZ 0
+#define B 4096 // Number of pencils in one batch
 #define NRANK 3
-#define DS1 2//downsample rate
-#define DS2 2 //downsample rate for z dim
-#define DS 2
+#define DS1 4//downsample rate
+#define DS2 4 //downsample rate for z dim
+#define DS 4
 #define TO_PRINT 0
 #define OCTREE_FINEST 4
